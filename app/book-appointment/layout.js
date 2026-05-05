8ad0c77a55f607a9f4a7c1378f18c1b76e24d1c5
Deepak <deepakbaradwaj933@gmail.com>
2026-05-05 15:11:41 +0530

    a

---
+++ app/book-appointment/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Book Appointment | Best IVF Centre in Delhi | Pravi IVF",
+  description: "Schedule your consultation with top fertility specialists at Pravi IVF. Take the first step towards your parenthood journey today.",
+  alternates: {
+    canonical: "https://praviglobalivf.com/book-appointment/",
+  },
+};
+
+export default function BookAppointmentLayout({ children }) {
+  return <>{children}</>;
+}
